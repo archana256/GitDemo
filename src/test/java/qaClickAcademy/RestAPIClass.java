89ef938c338a79ecf2a211bd85153fc179e886ff
archana <archana.bhalshinge72@gmail.com>
2022-07-16 16:09:02 +0530

    my first commit

---
+++ src/test/java/qaClickAcademy/RestAPIClass.java
@@ -0,0 +1,7 @@
+package qaClickAcademy;
+
+public class RestAPIClass {
+    public static void main(String args[]){
+        System.out.println("Test RestAPI framework");
+    }
+}
---
+++ src/test/java/qaClickAcademy/SeleniumTestcode.java
@@ -0,0 +1,7 @@
+package qaClickAcademy;
+
+public class SeleniumTestcode {
+    public static void main(String args[]){
+        System.out.println("Test selenium framework");
+    }
+}
