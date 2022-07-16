385693826d9313d0695e20abd43e057155b03d75
archana <archana.bhalshinge72@gmail.com>
2022-07-16 16:26:44 +0530

    This commit is from asia

--- src/test/java/qaClickAcademy/SeleniumTestcode.java
+++ src/test/java/qaClickAcademy/SeleniumTestcode.java
@@ -2,6 +2,9 @@ package qaClickAcademy;
 
 public class SeleniumTestcode {
     public static void main(String args[]){
+
         System.out.println("Test selenium framework");
+        System.out.println("Test selenium framework2");
+        System.out.println("Test selenium framework3");
     }
 }
